0f26474d463da32791736197a7ef9caaef46dc90
eastdh <eastdh@gmail.com>
2022-01-13 22:51:51 +0900

    EDIT simpleGallery - add btn click xml, edit code

--- study_Android/simpleGallary/app/src/main/java/com/example/simplegallary/MainActivity.java
+++ study_Android/simpleGallary/app/src/main/java/com/example/simplegallary/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.simplegallary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static  final  String TAG = "MultiImageActivity";
+    ArrayList<Uri> uriList = new ArrayList<>();//이미지의 uri를 담을 ArrayList 객체
+
+    RecyclerView recyclerView;
+    MultiImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //앨범으로 이동하는 버튼
+        Button btn_getImage = findViewById(R.id.getImage);
+        btn_getImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 2222);
+            }
+        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    //앨범에서 액티비티로 돌아온 후 실행되는 메서드
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data == null){//어떤 이미지도 선택하지 않은 경우
+            Toast.makeText(getApplicationContext(), "이미지를 선택하지 않았습니다.", Toast.LENGTH_LONG).show();
+        }
+        else{   //이미지를 하나라도 선택한 경우
+            if(data.getClipData() == null){ //이미지를 하나만 선택한 경우
+                
+            }
+
+        }
     }
 }
