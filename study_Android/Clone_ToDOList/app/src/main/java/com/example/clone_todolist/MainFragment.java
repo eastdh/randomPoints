c7ed5efd7e1258fae8c62ce2272b3ed26e9775f8
eastdh <eastdh@gmail.com>
2022-01-24 23:41:22 +0900

    EDIT Clone_ToDoList - NoteAdapter Clear

--- study_Android/Clone_ToDOList/app/src/main/java/com/example/clone_todolist/MainFragment.java
+++ study_Android/Clone_ToDOList/app/src/main/java/com/example/clone_todolist/MainFragment.java
@@ -3,11 +3,16 @@ package com.example.clone_todolist;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class MainFragment extends Fragment{
     private static final String TAG = "MainFragment";
+
+    RecyclerView recyclerView;
+    NoteAdapter adapter;
 
     @Override
     //화면을 생성할 때가 아닌 구성할 때, 즉 생성된 이후에 호출되는 역할.
@@ -24,6 +29,17 @@ public class MainFragment extends Fragment{
 
     //layoutManager와 어댑터를 이용해 리사이클러뷰를 사용하는 역할.
     private void initUI(ViewGroup rootView){
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        //fragment_main.xml에 만들었던 RecyclerView를 연결함.
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManger(layoutManager);
+        //LinearLayout에 recyclerView를 붙임.
+        //todo_item들이 세로로 하나하나 정렬하게하는 역할을 함
+
+        adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+        //어댑터들을 연결
 
     }
 }
