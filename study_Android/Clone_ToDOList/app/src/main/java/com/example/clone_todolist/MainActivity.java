bf14907e9ac89365d5cef5d20550081e3b53deaa
eastdh <eastdh@gmail.com>
2022-01-26 21:50:57 +0900

    EDIT Clone_ToDoList/MainActivity.java - saveTodo()

--- study_Android/Clone_ToDOList/app/src/main/java/com/example/clone_todolist/MainActivity.java
+++ study_Android/Clone_ToDOList/app/src/main/java/com/example/clone_todolist/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.clone_todolist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,10 +41,24 @@ public class MainActivity extends AppCompatActivity {
         openDatabase(); //onCreate()메서드 안에 openDatabase()가 있음으로써 앱이 실행될 때 실행됨
     }
 
+    EditText inputToDo;
+    Context context;
+
     public void saveToDo(){
         inputToDo = findViewById(R.id.inputToDo);
 
-        String todo = inputToDo.get
+        String todo = inputToDo.getText().toString();
+        //EditText에 적힌 글을 String todo로 가져옴
+
+        String sqlSave = "insert into " + NoteDatabase.TABLE_NOTE + " (TODO) values {"
+                + "'" + todo + "')";
+        //todo로 가져온 글을 sql문으로 만듦
+
+        NoteDatabase database = NoteDatabase.getInstance(context);
+        database.execSQL(sqlSave);
+        //execSQL로 sql문 실행, 가져온 글을 저장
+
+       inputToDo.setText("");//저장 후 빈 칸으로 만들기
 
     }
 
