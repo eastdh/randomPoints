84bb27c1cbc8ea8dd55c047987865a38a4c87a5f
eastdh <eastdh@gmail.com>
2022-01-28 23:29:45 +0900

    CREATE Chat.java

---
+++ study_Android/Clone_Messenger/app/src/main/java/com/example/clone_messenger/Chat.java
@@ -0,0 +1,22 @@
+package com.example.clone_messenger;
+
+public class Chat {
+    private String name;
+    private String msg;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
