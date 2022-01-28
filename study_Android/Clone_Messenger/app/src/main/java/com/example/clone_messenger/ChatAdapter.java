0991aba3bba1abdddef259e87121b20acbeae137
eastdh <eastdh@gmail.com>
2022-01-28 23:59:22 +0900

    CREATE ChatAdapter.java

---
+++ study_Android/Clone_Messenger/app/src/main/java/com/example/clone_messenger/ChatAdapter.java
@@ -0,0 +1,78 @@
+package com.example.clone_messenger;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> {
+
+    private List<Chat> chatList;
+    private String name;
+
+    //MainActivity.java에서 이 클래스를 쓸 수 있도록 ChatAdapter 메서드를 만들어 줌
+    public ChatAdapter(List<Chat> chatData, String name){
+        //MainActivity.java에서 받은 데이터들을 저장
+        chatList = chatData;
+        this.name = name;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView nameText;
+        public TextView msgText;
+        public View rootView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameText = itemView.findViewById(R.id.nameText);
+            msgText = itemView.findViewById(R.id.msgText);
+            rootView = itemView;
+
+            itemView.setEnabled(true);  //뷰 활성화
+            itemView.setClickable(true);//클릭 허용
+        }
+    }
+
+    @NonNull
+    @Override
+    public ChatAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item, parent, false);
+        //인플레이션
+
+        MyViewHolder myViewHolder = new MyViewHolder(linearLayout);
+        //객체를 새로 만듦
+
+        return myViewHolder;
+    }//ViewHolder가 실행될 때 진행되고 설정함
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatAdapter.MyViewHolder holder, int position) {
+
+        Chat chat = chatList.get(position);
+
+        holder.nameText.setText(chat.getName());
+        holder.msgText.setText(chat.getMsg());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatList == null ? 0: chatList.size();
+    }
+
+    public void addChat(Chat chat){
+        chatList.add(chat);
+        notifyItemInserted(chatList.size()-1);
+    }
+
+
+
+
+}
