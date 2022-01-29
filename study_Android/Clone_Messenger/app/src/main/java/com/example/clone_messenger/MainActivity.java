88df32234367b45388b5721439e3a9f9162086d2
eastdh <eastdh@gmail.com>
2022-01-29 23:34:47 +0900

    EDIT MainActivity.java - set sendButton, apply adapter on recyclerView

--- study_Android/Clone_Messenger/app/src/main/java/com/example/clone_messenger/MainActivity.java
+++ study_Android/Clone_Messenger/app/src/main/java/com/example/clone_messenger/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.clone_messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Chat> chatList;
+    private String nickname = "익명 1";
+
+    private EditText chatText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatText = findViewById(R.id.chatText);
+        sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String msg = chatText.getText().toString();
+
+                if(msg != null){
+                    Chat chat = new Chat();
+                    chat.setName(nickname);
+                    chat.setMsg(msg);
+
+                    chatText.setText("");
+
+                }
+            }
+        });
+
+        //리사이클러 뷰에 어댑터 적용
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        chatList = new ArrayList<>();
+        adapter = new ChatAdapter(chatList, nickname);
+        recyclerView.setAdapter(adapter);
     }
 }
