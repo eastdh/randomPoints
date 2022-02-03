e848189a96c53b5d73f30367ade6b0504888c95e
eastdh <eastdh@gmail.com>
2022-02-03 23:57:47 +0900

    Clone_Weather: How to get current time, EDIT README.md(Daily Log)

--- study_Android/Clone_Weather/README.md
+++ study_Android/Clone_Weather/README.md
@@ -7,7 +7,7 @@
 - Volley
 - JSON
 - Weather API
-
+------------
 2022-02-02   
 [UI 만들기](https://wikidocs.net/111271)   
 ConstraintLayout은 뷰의 연결점을 다른 뷰와 연결하여 이 뷰와 다른 뷰가 어떻게 연결 되는지 알려주는 제약 조건을 지님
@@ -18,3 +18,11 @@ ConstraintLayout의 장점
     >어느 뷰가 어디랑 연결되어 있는지 등..
 - RelativeLayout과 비슷하지만 더 유연하고 다양한 기능 제공
 - 뷰의 비율 조절에 용이
+----------------
+
+2022-02-03   
+UI 마무리   
+[시스템 시간 활용하기](https://wikidocs.net/111284)   
+System.currentTimeMillis()를 사용해 현재 시간을 밀리초 단위로 리턴 값을 받아온 후,  
+Date 클래스를 이용해 객체화 시키고,   
+SimpleDateFormat을 통해 다양한 형식으로 시간을 처리 함   
--- study_Android/Clone_Weather/app/src/main/java/com/example/clone_weather/MainActivity.java
+++ study_Android/Clone_Weather/app/src/main/java/com/example/clone_weather/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.clone_weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView dateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+
+        SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat simpleDateFormatTime = new SimpleDateFormat("HH:mm:ss");
+
+        String getDay = simpleDateFormatDay.format(date);
+        String getTime = simpleDateFormatTime.format(date);
+        String getDate = getDay + "\n" + getTime;
+
+        dateView = findViewById(R.id.dateView);
+
+        //이미지 버튼을 누르면 시간이 나올 수 있도록 함
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dateView.setText(getDate);
+
+            }
+        });
     }
+
 }
